a9f33fbab32479a894bfa77f1ffe1826fe93f8f9
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-18 10:05:34 -0600

    Add 'get' students, attendance, id

--- app/index.js
+++ app/index.js
@@ -1,1 +1,26 @@
+import express from "express";
 import GRADES from "./GRADES.js";
+
+
+const app = express();
+
+app.get("/api/students", (_, res) => {
+  res.json(STUDENTS);
+});
+
+app.get("/api/students/attendance", (_, res) => {
+  res.json(STUDENTS.map((student) => student.name));
+});
+
+app.get("/api/students/:id", (req, res) => {
+
+  const { id} = req.params;
+
+  const student = STUDENTS.find((student) => student.id === req.params.id);
+
+  res.send(student);
+});
+
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
