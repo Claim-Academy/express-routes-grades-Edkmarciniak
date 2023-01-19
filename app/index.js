3afa9e498875968adfaff90d2dd7d874678720e6
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-18 20:04:27 -0600

    Add app.get("/api/students", (_, res) => {});

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/students", (_, res) => {});
+
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
 });
