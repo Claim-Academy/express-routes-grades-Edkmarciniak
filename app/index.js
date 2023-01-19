88e009b1ef2d3d327a0e5c397561d9712897269c
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-19 15:05:37 -0600

    Correct line 8

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api/students", studentRoutes);
+app.use("/api/students/", studentRoutes);
 
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
