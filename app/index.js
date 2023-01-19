d0ede7cf9705168f728a6d7e958d3161a63bd797
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-19 14:49:14 -0600

    Remove app/ line 2

--- app/index.js
+++ app/index.js
@@ -1,10 +1,9 @@
 import express from "express";
-
-import studentRoutes from "./app/student/routes.js";
+import studentRoutes from "./student/routes.js";
 
 const app = express();
 
-app.use("api/students", studentRoutes);
+app.use(express.json());
 
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
