480f24ab9b3dba29aab2f12cd6293c37fb551897
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-18 20:12:13 -0600

    GET /api/students Returns all students and all their grades

--- app/index.js
+++ app/index.js
@@ -3,7 +3,9 @@ import STUDENTS from "./GRADES.js";
 
 const app = express();
 
-app.get("/api/students", (_, res) => {});
+app.get("/api/students", (_, res) => {
+  res.json(STUDENTS);
+});
 
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
