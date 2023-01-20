6c9bcf20e33244a7c34b670ba0c189dc7d43b916
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-20 12:22:52 -0600

    Updates routes

--- app/Student/Routes.js
+++ app/Student/Routes.js
@@ -3,54 +3,17 @@ import studentController from "./controller.js";
 
 const router = new Router();
 
+// /api/student
+// * DON'T REPEAT '/api/students' - it's already in app/index.js
 router.get("/", (_, res) => {
-  res.json(STUDENTS);
-});
-
-router.get("/attendance", (_, res) => {
-  res.json(STUDENTS.map((student) => student.name));
-});
-
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-
-  const student = STUDENTS.find((student) => student.id === Number(id));
-
-  if (student) {
-    res.json(student);
-  } else {
-    res.status(404).json({ message: "Student not found" });
-  }
-});
-
-router.post("/", (req, res) => {
-  const newStudent = req.body;
-
-  if (newStudent.name) {
-    STUDENTS.push({ ...newStudent, grades: [] });
-
-    res.json(STUDENTS);
-  } else {
-    res.status(400).json({ message: "Student name is required" });
-  }
-});
-
-router.put("/:id/grades", (req, res) => {
-  const { id } = req.params;
-
-  const student2Update = STUDENTS.find((student) => student.id === Number(id));
-
-  if (student2Update) {
-    student2Update.grades.push(req.body);
-
-    res.json(student2Update);
-  } else {
-    res.status(404).json({ message: "Student not found" });
-  }
-});
-
-router.delete("/:id", (req, res) => {
-  res.json(STUDENTS.filter((student) => student.id !== Number(req.params.id)));
+  studentController
+    .getStudents()
+    .then((students) => {
+      res.json(students);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
 });
 
 export default router;
