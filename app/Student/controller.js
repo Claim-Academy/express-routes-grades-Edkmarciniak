b8fd16e12224fafd8da4523f7eb4bbd32436dd3a
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-23 09:45:23 -0600

    Update controller.js

--- app/Student/controller.js
+++ app/Student/controller.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import config from "../config.js";
-import Student from "./Student.js";
 
 // Connect to the database
 mongoose
@@ -11,9 +10,3 @@ mongoose
   .catch((err) => {
     console.log("Error connecting to the database", err);
   });
-
-export default {
-  getStudents() {
-    return Student.find();
-  },
-};
