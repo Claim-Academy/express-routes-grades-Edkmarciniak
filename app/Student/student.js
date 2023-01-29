692291a8d5888e2ccbcf5d782c9ad50363fe6a2c
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-29 15:40:35 -0600

    Add student schema

--- app/Student/student.js
+++ app/Student/student.js
@@ -1,8 +1,11 @@
 import { Schema, model } from "mongoose";
-//
+// TODO: Create a schema for grades (https://github.com/manavm1990/node-mongo-mongoose-sample/blob/main/app/grade/Grade.js)
+// TODO: Add SOME validation to the schema
 export default model(
   "Student",
   new Schema({
-    name: String,
+    name: { type: String,
+            required: [true, "Name is required" ] },
+    grades: [{ type: [Grade.schema], default: [] },
   })
 );
