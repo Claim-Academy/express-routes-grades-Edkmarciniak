88ef2496c43e8c50be3a94ecc0a34566685c34d5
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-29 15:53:02 -0600

    Add TODO list

--- app/Student/controller.js
+++ app/Student/controller.js
@@ -2,19 +2,34 @@ import mongoose from "mongoose";
 import config from "../config.js";
 import Student from "./Student.js";
 
+mongoose.set("strictQuery", true);
 mongoose
   .connect(config.getDbConn("students"))
   .then(() => {
-    console.log("Connected to the DB");
+    console.info("Connected to the database");
   })
   .catch((err) => {
-    console.log("Error connecting to DB", err);
+    console.error("Error connecting to the database", err);
   });
 
-export default {
+export const controller = {
   getStudents() {
     return Student.find();
   },
 
+  // TODO: Set up the corresponding route in app/student/routes.js 👇🏾
 
+  // TODO: Add method to get a single student by id
 
+  // TODO: Add method to create a new student (scores can be empty)
+
+  // TODO: Add method to update a single student's name by id
+
+  // TODO: Add method to update a single score by student id and score id
+
+  // TODO: Add method to delete a single score by student id and score id
+
+  // TODO: Add method to delete a single student by id
+};
+
+export default controller;
