84e812f2552a0f3fc5b2415b738b4ee386237e15
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-04 11:34:46 -0600

    Update server.js and index.js

--- app/index.js
+++ app/index.js
@@ -1,16 +1,5 @@
-import express from "express";
-import studentRoutes from "./student/routes.js";
+import initDb from "./db.js";
+import initServer from "./server.js";
 
-const app = express();
-
-// Tell express to parse the request body as JSON
-// Without this, req.body will be undefined
-// * THIS MIDDLEWARE MUST BE BEFORE THE ROUTES
-app.use(express.json());
-
-// Any requests to /api/students will be handled by studentRoutes
-app.use("/api/students/", studentRoutes);
-
-app.listen(3000, () => {
-  console.info("Server is running on port 3000");
-});
+initDb();
+initServer();
